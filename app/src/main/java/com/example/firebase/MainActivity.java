90dcda6373737b7cfaee030518200440360258cb
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-07-06 09:16:43 -0500

    inicio3

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.firebase;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -15,16 +17,41 @@ public class MainActivity extends AppCompatActivity {
 
     EditText txtLogin , txtClave;
     FirebaseAuth mAuth;
+    Button btnLogin, btnCrear;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        txtLogin = findViewById(R.id.txtLogin);
+        txtClave = findViewById(R.id.txtClave);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCrear = findViewById(R.id.btnCrear);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnCrear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
